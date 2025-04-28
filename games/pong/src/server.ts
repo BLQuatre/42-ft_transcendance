3469a1fd06ba300e05fd8380e8c46342f2d705c7
ft-NotArt <noterisarthur42@gmail.com>
2025-04-28 21:21:47 +0200

    fix(pong): websocket run

--- games/pong/src/server.ts
+++ games/pong/src/server.ts
@@ -1,48 +1,43 @@
 import Fastify from 'fastify';
-import fastifyWebsocket from '@fastify/websocket';
-import { Game } from './game';
+import { WebSocketServer, WebSocket } from 'ws'; // <-- Correct
 
 const fastify = Fastify();
-fastify.register(fastifyWebsocket);
+const connections: WebSocket[] = []; // <-- Correct type
 
-const connections: any[] = [];
-const game = new Game();
-
-// Handle WebSocket connection
-fastify.get('/ws', { websocket: true }, (connection, req) => {
-	console.log('Client connected');
-
-	connections.push(connection);
-
-	connection.socket.on('message', (message: Buffer) => {
-		const input = JSON.parse(message.toString());
-		handleInput(input, connection);
-	});
-
-	connection.socket.on('close', () => {
-		console.log('Client disconnected');
-		const index = connections.indexOf(connection);
-		if (index > -1) connections.splice(index, 1);
-	});
+fastify.get('/', async (request, reply) => {
+	return { pong: 'it works' };
 });
 
-// Move paddle based on client input
-function handleInput(input: any, connection: any) {
-	const { action, direction, player } = input;
-	if (action === 'move') {
-		const paddle = player === 1 ? game.state.paddle1 : game.state.paddle2; // FIXME: this is implying 2 player only
-		if (direction === 'up') paddle.y -= 5;
-		if (direction === 'down') paddle.y += 5;
-	}
-}
-
-// Game loop
-setInterval(() => {
-	game.update();
-	const state = JSON.stringify(game.state);
-	connections.forEach(conn => conn.socket.send(state));
-}, 1000 / 60);
-
-fastify.listen({ port: 3000 }, () => {
+const start = async () => {
+	await fastify.listen({ port: 3000 });
 	console.log('Server running on http://localhost:3000');
-});
+
+	const wss = new WebSocketServer({ server: fastify.server });
+
+	wss.on('connection', (ws) => {
+		console.log('Client connected');
+		connections.push(ws);
+
+		ws.on('message', (message) => {
+			console.log('Received message:', message.toString());
+		});
+
+		ws.on('close', () => {
+			console.log('Client disconnected');
+			const index = connections.indexOf(ws);
+			if (index > -1) connections.splice(index, 1);
+		});
+	});
+
+	setInterval(() => {
+		const gameState = { ball: { x: 0, y: 0 }, paddles: [] }; // example
+		const state = JSON.stringify(gameState);
+		connections.forEach(ws => {
+			if (ws.readyState === ws.OPEN) {
+				ws.send(state);
+			}
+		});
+	}, 1000 / 60);
+};
+
+start();

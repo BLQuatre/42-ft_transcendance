3fa7ea6ab0035fef0d8a39d185f126a74cb253c0
ft-NotArt <noterisarthur42@gmail.com>
2025-05-12 16:53:42 +0000

    feat(dino): cactus

--- games/dino/src/game.ts
+++ games/dino/src/game.ts
@@ -1,25 +1,62 @@
 import { Player } from './player' ;
+import { Obstacle } from './obstacle' ;
 
-const FPS = 10 ;
+const FPS = 30 ;
+const SCREEN_WIDTH = 800 ;
+const CACTUSWIDTH = 30 ;
+const MIN_OBSTCL_SPAN = 6 ;
+const DIFFICULTY_COEFF = 100 ;
 
 export class Game {
-	currentScore: number ; // Ramping up constantly, attributed to a player when he "loses"
+	score: number ; // Ramping up constantly, attributed to a player when he "loses"
 	dinos: { player: Player, score: number }[] ;
-
+	obstacles: Obstacle[] ;
+	
+	private frameCount = 0 ;
+	private lastWasClose = false ;
 
 	constructor() {
-		this.currentScore = 0 ;
+		this.score = 0 ;
 		this.dinos = [] ;
+		this.obstacles = [] ;
 
 		this.autoUpdate() ;
 	}
 	
+	// should start updating upon request instead of constructor
 	private autoUpdate() {
 		const interval = 1000 / FPS ;
 	
 		setInterval(() => {
-			this.currentScore++ ;
+			if (this.frameCount === 0)
+				this.score++ ;
+			this.frameCount = ((this.frameCount + 1) % 3) ;
+
+			// see if a timeout before obstacles is needed
+			this.handleObstacle() ;
 		}, interval) ;
+	}
+	
+	private handleObstacle() {
+		if (this.canSpawnObstacle(this.obstacles.at(-1)?.get_x_pos() ?? 0) && Math.random() <= 0.01 + (Math.log(1 + this.score / (1000 * DIFFICULTY_COEFF))))
+			this.obstacles.push(new Obstacle(this.score)) ;
+	
+		this.obstacles = this.obstacles.filter(obstacle => obstacle.get_x_pos() >= 0); // Erase off-screen obstacles
+	}
+	
+
+	private canSpawnObstacle(lastObstaclePos: number): boolean {
+		const canSpawnFar = lastObstaclePos < SCREEN_WIDTH - MIN_OBSTCL_SPAN * CACTUSWIDTH;
+		const canSpawnClose = (
+			lastObstaclePos < SCREEN_WIDTH - CACTUSWIDTH &&
+			lastObstaclePos > SCREEN_WIDTH - 2 * CACTUSWIDTH &&
+			!this.lastWasClose
+		) ;
+
+		if		(canSpawnClose)	this.lastWasClose = true ;
+		else if	(canSpawnFar)	this.lastWasClose = false ;
+
+		return canSpawnFar || canSpawnClose ;
 	}
 
 
@@ -32,6 +69,9 @@ export class Game {
 			dinos: this.dinos.map(dino => ({
 				y: dino.player.get_y_pos(),
 				score: dino.score
+			})),
+			obstacles: this.obstacles.map(obstacle => ({
+				x: obstacle.get_x_pos()
 			}))
 		}
 	}
---
+++ games/dino/src/obstacle.ts
@@ -0,0 +1,33 @@
+const FPS = 30 ;
+const SCREEN_WIDTH = 800 ;
+const OBSTCL_BASE_SPD = 3 ;
+const SPD_SCALE_DIV = 30 ;
+
+export class Obstacle {
+	private x_pos: number ;
+	private score: number ; // Used to know how fast the obstacle should move
+
+	private frameCount = 0 ;
+
+
+	constructor(score: number) {
+		this.x_pos = SCREEN_WIDTH ;
+		this.score = score ;
+
+		this.autoUpdate() ;
+	}
+
+	private autoUpdate() {
+		const interval = 1000 / FPS ;
+	
+		setInterval(() => {
+			if (this.frameCount === 0) this.score++ ;
+			this.frameCount = ((this.frameCount + 1) % 3) ;
+	
+			this.x_pos -= OBSTCL_BASE_SPD + OBSTCL_BASE_SPD * Math.log(1.3 + (this.score / SPD_SCALE_DIV)) ;
+		}, interval) ;
+	}
+
+
+	get_x_pos() { return this.x_pos ; }
+}

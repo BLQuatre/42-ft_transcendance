275c2184c4e45854ffb97add81dc9d4b5421f696
kaveOO <alban.billiette@gmail.com>
2025-05-14 05:45:35 +0200

    feat(shop): added metadata title

---
+++ frontend/app/[lang]/shop/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Shop - Transcendance',
+}
+
+export default function RegisterLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}

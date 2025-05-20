ce58e095b350e4b45067a33bef10967fd8ae790e
BL4 <clem29auv@gmail.com>
2025-05-20 18:20:54 +0200

    fix: function name mistake

--- services/auth-service/src/controllers/2fa.controllers.ts
+++ services/auth-service/src/controllers/2fa.controllers.ts
@@ -1,5 +1,4 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { getenvVar } from "../utils/functions";
 import dotenv from 'dotenv';
 import path from 'path';
 import speakeasy from 'speakeasy';
@@ -8,5 +7,5 @@ import qrcode from 'qrcode';
 dotenv.config({ path: path.resolve(__dirname, '../../../.env')})
 
 export const tfaSetup = async (req: FastifyRequest, reply: FastifyReply) => {
-    
+
 }

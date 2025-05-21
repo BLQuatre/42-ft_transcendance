d57a03acf44b034f2a03c3ead521c9ed9a4b09ef
ft-NotArt <noterisarthur42@gmail.com>
2025-05-21 22:48:39 +0200

    fix(pong): wrong import after rebase

--- frontend/components/WaitingRoom.tsx
+++ frontend/components/WaitingRoom.tsx
@@ -6,7 +6,7 @@ import Image from "next/image"
 import { Users, CheckCircle, XCircle, Home, Copy, Check } from "lucide-react"
 import { Button } from "@/components/ui/Button"
 import { Badge } from "@/components/ui/Badge"
-import { cn } from "@/lib/Utils"
+import { cn } from "@/lib/utils"
 
 // Types
 export type Player = {

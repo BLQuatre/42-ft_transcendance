d35ab4affb916d6e42805c12d595e8acee24c853
kaveOO <alban.billiette@gmail.com>
2025-05-21 04:14:48 +0100

    fix(app/layout.tsx): applied the Inter font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import '../styles/globals.css'  // Import your global styles
+
+// Initialize the Inter font
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: 'Transcendance',
@@ -18,7 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className={inter.className}>
         {children}
       </body>
     </html>

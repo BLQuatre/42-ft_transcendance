d32176248af6e28fed9046bc0b91b4a4a72b80fb
BL4 <clem29auv@gmail.com>
2025-05-26 13:55:32 +0200

    feat: working

--- proxy/tools/entrypoint.sh
+++ proxy/tools/entrypoint.sh
@@ -3,6 +3,6 @@
 # envsubst '${GATEWAY_HOST} ${GATEWAY_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # TMP
-envsubst '${GATEWAY_HOST} ${GATEWAY_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT} ${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${GATEWAY_HOST} ${GATEWAY_PORT} ${FRONTEND_HOST} ${FRONTEND_PORT} ${MAIN_PORT} ${MAIN_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
